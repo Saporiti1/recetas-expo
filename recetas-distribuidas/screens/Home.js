b1f0e1cb9acc9979e69de2cffa9ac9e9d0b148d3
Astrovik3 <juantomaz13@gmail.com>
2022-07-02 22:15:40 -0300

    back en Login y Home sin imagenes

--- recetas-distribuidas/screens/Home.js
+++ recetas-distribuidas/screens/Home.js
@@ -1,201 +1,229 @@
-import React, { useState } from 'react';
-import { View, Text, Image, TextInput, StyleSheet, Alert, Pressable, Button, TouchableOpacity, ScrollView, StatusBar } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, StyleSheet, Alert, Pressable, TouchableOpacity, ScrollView, StatusBar } from 'react-native';
 import { Box, Icon, IconComponentProvider } from "@react-native-material/core";
-import { Card, CardContent, CardAction, CardImage } from 'react-native-material-cards';
+import { Card, CardAction, CardImage } from 'react-native-material-cards';
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import { useNavigation } from '@react-navigation/native';
 
 
 import NavBarSup from '../components/NavBarSup';
 import NavBarInf from '../components/NavBarInf';
+import { searchRecipes } from '../utils/recipesAPI';
 
 
 const starImgFilled = 'https://github.com/tranhonghan/images/blob/main/star_filled.png?raw=true';
 const starImgCorner = 'https://github.com/tranhonghan/images/blob/main/star_corner.png?raw=true';
 
+
 const Home = () => {
-    const [defaultRating, setDefaultRating] = useState(4);
-    const [maxRating, setMaxRating] = useState([1, 2, 3, 4, 5]);
+  const navigation = useNavigation();
+  //const [defaultRating, setDefaultRating] = useState(4);
+  const [maxRating, setMaxRating] = useState([1, 2, 3, 4, 5]);
 
-    const navigation = useNavigation();
 
+  const [foodData, setFoodData] = useState('');
+  const [userData, setUserData] = useState('');
+  const [foodRating, setRating] = useState(0);
+  const [recipeOne, setRecipeOne] = useState('');
+  const [recipeTwo, setRecipeTwo] = useState('');
+  const [recipeThree, setRecipeThree] = useState('');
 
-    const CustomRatingBar = () => {
-        const [recipesScroll, setRecipesScroll] = useState([]);
+  useEffect(() =>{
+    const validateUser = async () => {
+      const recipeDataApi = await searchRecipes()
+      .then(
+        (result) => {
+          setFoodData(result[0]);
+          setUserData(result[0].user);
+          //NO ME LA ESTÁ CARGANDO LA PRIMERA VEZ... 
+          //Y POR ESO SIEMPRE ROMPE AL COMIENZO... POR ESO SEPARO EN 3
+          //setRecipesScroll(result.slice(0,3));
 
-        return (
-            <View style={styles.customRatingBarStyle}>
-                {
-                    maxRating.map((item, key) => {
-                        return (
-                            <TouchableOpacity
-                                activeOpactity={0.7}
-                                key={item}
-                            >
-                                <Image
-                                    style={styles.starImgStyle}
-                                    source={
-                                        item <= defaultRating
-                                            ? { uri: starImgFilled }
-                                            : { uri: starImgCorner }
-                                    }
-                                />
-                            </TouchableOpacity>)
-                    })
-                }
-            </View>
-        )
+          //NO DEBERÍA DIVIDIRLO EN 3... PERO BUENO...
+          setRecipeOne(result[0]);
+          setRecipeTwo(result[1]);
+          setRecipeThree(result[2]);
+
+          const avgRating = result[0].ratingSet.map(item => item.rating).reduce((a, b) => a + b, 0);
+          setRating(Math.round(avgRating / result[0].ratingSet.length));
+      });
+
+      //setFoodData(recipeDataApi[0]);
     }
+    validateUser();
+  }, []);
+
+  //<Text>{recipesScroll[0].name}</Text>
+
+  const CustomRatingBar = () => {
+
     return (
-        <View style={styles.container}>
-            <NavBarSup />
-        
-            <Text style={styles.novedades}>Novedades</Text>
+      <View style={styles.customRatingBarStyle}>
+        {
+          maxRating.map((item, key) => {
+            return (
+              <TouchableOpacity
+                activeOpactity={0.7}
+                key={item}
+              >
+                <Image
+                  style={styles.starImgStyle}
+                  source={
+                    item <= foodRating
+                      ? { uri: starImgFilled }
+                      : { uri: starImgCorner }
+                  }
+                />
+              </TouchableOpacity>)
+          })
+        }
+      </View>
+    ) 
+  }
 
-            <View>
-                <Box style={{
-                    display: 'flex',
-                    width: 'auto',
-                    height: 300,
-                    top: 3,
-                    backgroundColor: '#FCDC8C',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center'
-                }}>
-                    <Card style={{ margin: 10, borderRadius: 20, backgroundColor: 'grey', width: '100%' }} onPress={() => navigation.navigate('Receta')}>
-                        <CardImage
-                            source={{ uri: 'https://cdn.pixabay.com/photo/2016/08/31/17/41/sunrise-1634197_960_720.jpg' }}
-                            style={{ borderTopLeftRadius: 20, borderTopRightRadius: 20, width: '98%' }}
-                            
-                        />
-                        <Text >Ensalada 7 elemntos</Text>
-                        <Text >Por roberto carlos</Text>
-                        <CardAction separator={false} inColumn={false} style={{ justifyContent: 'space-between' }}>
-                            <CustomRatingBar />
+  return (
+    <View style={styles.container}>
+      <NavBarSup />
 
-                            <Icon name="bookmark-outline" size={25} style={{ marginRight: 10, color: '#F1AE00' }} />
+      <Text style={styles.novedades}>Novedades</Text>
 
-                        </CardAction>
-                    </Card>
-                </Box>
-            </View>
+      <View>
+        <Box style={{
+          display: 'flex',
+          width: 'auto',
+          height: 300,
+          top: 3,
+          backgroundColor: '#FCDC8C',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}>
+          <Card style={{margin: 10, borderRadius: 20, backgroundColor: 'grey', width: '100%'}} onPress={() => navigation.navigate('Receta')}>
+            <CardImage
+              //source={foodData.photoUrl}
+              source={{uri: 'https://cdn.pixabay.com/photo/2016/08/31/17/41/sunrise-1634197_960_720.jpg' }}
+              style={{borderTopLeftRadius: 20, borderTopRightRadius: 20, width: '98%' }}
 
-            <View >
-                <Text style={styles.novedades}>Recomendaciones</Text>
+            />
+            <Text>{`${foodData.name}`}</Text>
+            <Text>{'por ' + `${userData.name}`}</Text>
+            <CardAction separator={false} inColumn={false} style={{justifyContent: 'space-between'}}>
+              <CustomRatingBar />
 
-                <View>
-                    <Box style={styles.scrollHorizontal}>
-                        <ScrollView scrollEventThrottle={16}>
-                            <View style={{ height: 170 }}>
-                                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+              <Icon name="bookmark-outline" size={25} style={{marginRight: 10, color: '#F1AE00'}}/>
 
-                                    <View style={{ height: 170, width: 170, marginLeft: 20, borderWidth: 0.5, borderColor: '#dddddd' }}>
-                                        <View style={{ flex: 2 }}>
-                                            <Image
-                                                source={require('../media/imgLogin.png')}
-                                                style={{ flex: 1, width: null, height: null, resizeMode: 'cover' }}
-                                            />
-                                        </View>
-                                        <View style={{ flex: 1, paddingLeft: 20, backgroundColor: 'grey' }}>
-                                            <Text>Home</Text>
-                                        </View>
-                                    </View>
+            </CardAction>
+          </Card>
+        </Box>
+      </View>
 
-                                    <View style={{ height: 170, width: 170, marginLeft: 20, borderWidth: 0.5, borderColor: '#dddddd' }}>
-                                        <View style={{ flex: 2 }}>
-                                            <Image
-                                                source={require('../media/imgLogin.png')}
-                                                style={{ flex: 1, width: null, height: null, resizeMode: 'cover' }}
-                                            />
-                                        </View>
-                                        <View style={{ flex: 1, paddingLeft: 20, backgroundColor: 'grey' }}>
-                                            <Text>buenas</Text>
-                                        </View>
-                                    </View>
+      <View >
+        <Text style={styles.novedades}>Recomendaciones</Text>
 
-                                    <View style={{ height: 170, width: 170, marginLeft: 20, borderWidth: 0.5, borderColor: '#dddddd' }}>
-                                        <View style={{ flex: 2 }}>
-                                            <Image
-                                                source={require('../media/imgLogin.png')}
-                                                style={{ flex: 1, width: null, height: null, resizeMode: 'cover' }}
-                                            />
-                                        </View>
-                                        <View style={{ flex: 1, paddingLeft: 20, backgroundColor: 'grey' }}>
-                                            <Text>Home</Text>
-                                        </View>
-                                    </View>
-                                    <View style={{ height: 170, width: 170, marginLeft: 20, borderWidth: 0.5, borderColor: '#dddddd' }}>
-                                        <View style={{ flex: 2 }}>
-                                            <Image
-                                                source={require('../media/imgLogin.png')}
-                                                style={{ flex: 1, width: null, height: null, resizeMode: 'cover' }}
-                                            />
-                                        </View>
-                                        <View style={{ flex: 1, paddingLeft: 20, backgroundColor: 'grey' }}>
-                                            <Text>Home</Text>
-                                        </View>
-                                    </View>
+        <View>
+          <Box style={styles.scrollHorizontal}>
+            <ScrollView scrollEventThrottle={16}>
+              <View style={{height: 170}}>
+                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
 
-                                </ScrollView>
+                  <View style={{height: 170, width: 170, marginLeft: 20, borderWidth: 0.5, borderColor: '#dddddd' }}>
+                    <View style={{flex: 2}}>
+                      <Image
+                        //source={recipeOne.photoUrl}
+                        source={require('../media/imgLogin.png')}
+                        style={{flex: 1, width: null, height: null, resizeMode: 'cover'}}
+                      />
+                    </View>
+                    <View style={{flex: 1, paddingLeft: 20, backgroundColor: 'grey'}}>
+                      <Text>{recipeOne.name}</Text>
+                    </View>
+                  </View>
 
-                            </View>
+                  <View style={{height: 170, width: 170, marginLeft: 20, borderWidth: 0.5, borderColor: '#dddddd' }}>
+                    <View style={{flex: 2}}>
+                      <Image
+                        //source={recipeTwo.photoUrl}
+                        source={require('../media/imgLogin.png')}
+                        style={{flex: 1, width: null, height: null, resizeMode: 'cover'}}
+                      />
+                    </View>
+                    <View style={{flex: 1, paddingLeft: 20, backgroundColor: 'grey'}}>
+                      <Text>{recipeTwo.name}</Text>
+                    </View>
+                  </View>
 
-                        </ScrollView>
-                    </Box>
-                </View>
-            </View>
-            <NavBarInf />
+                  <View style={{height: 170, width: 170, marginLeft: 20, borderWidth: 0.5, borderColor: '#dddddd' }}>
+                    <View style={{flex: 2}}>
+                      <Image
+                        //source={recipeThree.photoUrl}
+                        source={require('../media/imgLogin.png')}
+                        style={{flex: 1, width: null, height: null, resizeMode: 'cover'}}
+                      />
+                    </View>
+                    <View style={{flex: 1, paddingLeft: 20, backgroundColor: 'grey'}}>
+                      <Text>{recipeThree.name}</Text>
+                    </View>
+                  </View>
+
+                </ScrollView>
+
+              </View>
+
+            </ScrollView>
+          </Box>
         </View>
-    )
+      </View>
+      <NavBarInf />
+    </View>
+  )
 };
 export default () => (
-    <IconComponentProvider IconComponent={MaterialCommunityIcons}>
-        <Home />
-    </IconComponentProvider>
+  <IconComponentProvider IconComponent={MaterialCommunityIcons}>
+    <Home />
+  </IconComponentProvider>
 );
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: StatusBar.currentHeight
-    },
-    text: {
-        fontSize: 25,
-        fontWeight: '500',
-    },
-    novedades: {
-        paddingLeft: 15,
-        marginTop: 3,
-        marginBottom: 0,
-        height: 25,
-        fontSize: 22,
-        color: '#1E1E1E',
-    },
-    customRatingBarStyle: {
-        justifyContent: 'center',
-        flexDirection: 'row',
-        marginLeft: 10
-    },
-    starImgStyle: {
-        width: 20,
-        height: 20,
-        resizeMode: 'cover',
-    },
-    scrollHorizontal: {
-        width: 'auto',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: 200,
-        display: 'flex',
-        position: 'relative',
-        paddingLeft: 15,
-        horizontal: 'true',
-        gap: 1,
-        decelerationRate: 0,
-        snapToInterval: 200, //your element width
-        snapToAlignment: 'center',
-        '::-webkit-scrollbar': { display: 'none' },
-    }
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight
+  },
+  text: {
+    fontSize: 25,
+    fontWeight: '500',
+  },
+  novedades: {
+    paddingLeft: 15,
+    marginTop: 3,
+    marginBottom: 0,
+    height: 25,
+    fontSize: 22,
+    color: '#1E1E1E',
+  },
+  customRatingBarStyle: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginLeft: 10
+  },
+  starImgStyle: {
+    width: 20,
+    height: 20,
+    resizeMode: 'cover',
+  },
+  scrollHorizontal: {
+    width: 'auto',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 200,
+    display: 'flex',
+    position: 'relative',
+    paddingLeft: 15,
+    horizontal: 'true',
+    gap: 1,
+    decelerationRate: 0,
+    snapToInterval: 200, //your element width
+    snapToAlignment: 'center',
+    '::-webkit-scrollbar': { display: 'none' },
+  }
 });
--- recetas-distribuidas/screens/Login.js
+++ recetas-distribuidas/screens/Login.js
@@ -1,118 +1,143 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, Image, TextInput, SafeAreaView, StyleSheet, Alert, Pressable, StatusBar } from 'react-native';
+
+
+import { loginUser } from '../utils/recipesAPI';
 
 
 
 const Login = ({ navigation }) => {
-    const [text, onChangeText] = useState("");
-    const [textPass, onChangeTextPass] = useState("");
+  const [textUser, onChangeTextUser] = useState("");
+  const [textPass, onChangeTextPass] = useState("");
 
-    return (
-        <View style={styles.container}>
-            <SafeAreaView style={{height: '100%'}}>
-                <Image source={require('../media/imgLogin.png')} style={{ width: '100%', height: 250 }} />
+  //const [aviso, setAviso] = useState('');
 
-                <View style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    height: '100%'
-                }}>
-                    <View style={{
-                        display: 'flex',
-                        width: '80%',
-                        marginTop: 10,
-                        height: 150,
-                        flexDirection: 'column',
-                        justifyContent: 'space-around'
-                    }}>
-                        <SafeAreaView style={{top: 30}}>
-                            <TextInput
-                                style={styles.input}
-                                onChangeText={onChangeText}
-                                value={text}
-                                placeholder="Usuario"
-                            />
-                            <TextInput
-                                style={styles.input}
-                                onChangeText={onChangeTextPass}
-                                value={textPass}
-                                secureTextEntry={true}
-                                placeholder="Contraseña"
-                            />
-                        </SafeAreaView>
-                    </View>
 
-                    <Pressable onPress={() => navigation.navigate('ResetPassword')} title="Login" >
-                        <Text style={styles.textMeOlvideContra} >Me olvide la Contraseña</Text>
-                    </Pressable>
+  //ACA NO LE DOY CON EL USE EFFECT PERO FUNCIONA BIEN... IGUAL VERLO BIEN...
+  const validateUser = async () => {
+    const userDataAPI = await loginUser(textUser, textPass);
 
-                    <View style={{display: 'flex', height: '100%'}}>
-                        <View style={{display: 'flex', marginTop: 50}}>
-                            <Pressable onPress={() => navigation.navigate('Home')} title="Login" style={styles.bttnLogin} >
-                                <Text style={styles.textLogin} >Login</Text>
-                            </Pressable>
-                        </View>
+    //console.log(userDataAPI);
+    if(userDataAPI == 200) {
+      //setAviso(<Alert severity="success">Todo en orden</Alert>);
+      setTimeout(() => {
+        navigation.navigate('Home');
+      }, 1000);
+    } 
+    else {
+      /*setAviso(<Alert severity="error">El usuario no existe</Alert>);
+      setTimeout(() => {
+        setAviso('');
+      }, 1500);*/
+    }
+  }
 
-                        <View style={styles.fixToText}>
-                            <Text>No tenes cuenta?</Text>
-                            <Pressable onPress={() => navigation.navigate('NewAccount')} title="Login" >
-                                <Text style={styles.textCrear}>  Crear</Text>
-                            </Pressable>
-                        </View>
-                    </View>
-                </View>
+  return (
+    <View style={styles.container}>
+      <SafeAreaView style={{ height: '100%' }}>
+        <Image source={require('../media/imgLogin.png')} style={{ width: '100%', height: 250 }} />
+
+        <View style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          height: '100%'
+        }}>
+          <View style={{
+            display: 'flex',
+            width: '80%',
+            marginTop: 10,
+            height: 150,
+            flexDirection: 'column',
+            justifyContent: 'space-around'
+          }}>
+            <SafeAreaView style={{ top: 30 }}>
+              <TextInput
+                style={styles.input}
+                onChangeText={onChangeTextUser}
+                value={textUser}
+                placeholder="Usuario"
+              />
+              <TextInput
+                style={styles.input}
+                onChangeText={onChangeTextPass}
+                value={textPass}
+                secureTextEntry={true}
+                placeholder="Contraseña"
+              />
             </SafeAreaView>
-        </View >
-    );
+          </View>
+
+          <Pressable onPress={() => navigation.navigate('ResetPassword')} title="Login" >
+            <Text style={styles.textMeOlvideContra}>Me olvide la Contraseña</Text>
+          </Pressable>
+
+          <View style={{ display: 'flex', height: '100%' }}>
+            <View style={{ display: 'flex', marginTop: 50 }}>
+              <Pressable onPress={validateUser} title="Login" style={styles.bttnLogin} >
+                <Text style={styles.textLogin} >Login</Text>
+              </Pressable>
+            </View>
+
+            <View style={styles.fixToText}>
+              <Text>No tenes cuenta?</Text>
+              <Pressable onPress={() => navigation.navigate('NewAccount')} title="Login" >
+                <Text style={styles.textCrear}>  Crear</Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </SafeAreaView>
+    </View >
+  );
 }
 export default Login;
 
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-        paddingTop: StatusBar.currentHeight
-    },
-    input: {
-        height: 40,
-        margin: 12,
-        borderWidth: 1,
-        padding: 10,
-    },
-    bttnLogin: {
-        marginTop: 80,
-        width: 200,
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingVertical: 12,
-        paddingHorizontal: 32,
-        borderRadius: 15,
-        backgroundColor: '#F1AE00',
-    },
-    textLogin: {
-        fontSize: 20,
-        lineHeight: 21,
-        fontWeight: 'bold',
-        letterSpacing: 0.25,
-        color: 'white',
-    },
-    textMeOlvideContra: {
-        fontWeight: 'bold',
-        color: '#F1AE00',
-        display: 'flex',
-        marginTop: 20,
-        marginLeft: 150,
-    },
-    fixToText: {
-        display: 'flex',
-        justifyContent: 'center',
-        flexDirection: 'row',
-        marginTop: 15,
-    },
-    textCrear: {
-        fontWeight: 'bold',
-        color: '#F1AE00',
-    },
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    paddingTop: StatusBar.currentHeight
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  bttnLogin: {
+    marginTop: 80,
+    width: 200,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 15,
+    backgroundColor: '#F1AE00',
+  },
+  textLogin: {
+    fontSize: 20,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+  textMeOlvideContra: {
+    fontWeight: 'bold',
+    color: '#F1AE00',
+    display: 'flex',
+    marginTop: 20,
+    marginLeft: 150,
+  },
+  fixToText: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginTop: 15,
+  },
+  textCrear: {
+    fontWeight: 'bold',
+    color: '#F1AE00',
+  },
 });
