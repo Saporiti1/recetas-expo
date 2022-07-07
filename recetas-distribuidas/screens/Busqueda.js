954487b32c1adb7853b97e0ac1fb43e115f4d486
Augusto Saporiti <augsapo57@gmail.com>
2022-07-07 11:24:33 -0300

    Agrego el import en busqueda

--- recetas-distribuidas/screens/Busqueda.js
+++ recetas-distribuidas/screens/Busqueda.js
@@ -1,6 +1,6 @@
 import React, { useState, Component } from 'react';
 import { View, Text, Image, StyleSheet, FlatList, TouchableOpacity, StatusBar } from 'react-native';
-import { Stack, Icon, IconComponentProvider, TextInput } from "@react-native-material/core";
+import { Stack, Icon, IconComponentProvider, TextInput, IconButton } from "@react-native-material/core";
 import MaterialCommunityIcons from "@expo/vector-icons/Ionicons";
 import Picker from 'react-native-picker-select';
 
