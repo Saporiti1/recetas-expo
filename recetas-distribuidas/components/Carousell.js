647c359224ae4df2b1b0df2a49233c92e76a245a
Astrovik3 <juantomaz13@gmail.com>
2022-07-12 19:38:34 -0300

    ver carousel no renderiza elementos

--- recetas-distribuidas/components/Carousell.js
+++ recetas-distribuidas/components/Carousell.js
@@ -1,14 +1,18 @@
 import * as React from 'react';
 import { Text, View, SafeAreaView, Image } from 'react-native';
 import Carousel from 'react-native-snap-carousel';
+//import { IconComponentProvider } from "@react-native-material/core";
+//import MaterialCommunityIcons from "@expo/vector-icons/Ionicons";
 
 
 const Carousell = ({pasos}) => {
+  //const route = useRoute();
+  //const pasos = route.params.pasos;
 
 
   //source={{uri: item.multimediaSet[0].contentUrl}}
 
-  const renderItem = ({ item, index }) => {
+  const renderItem = ({item}) => {
     return (
       <View style={{
         backgroundColor: 'floralwhite',
@@ -18,26 +22,37 @@ const Carousell = ({pasos}) => {
         marginLeft: 20,
         marginRight: 20,
       }}>
-        <Image source={require('../media/imgLogin.png')} style={{ width: '100%', height: '50%', borderRadius: 5 }} />
-        <Text style={{ fontSize: 30 }}>{item.stepNumber}</Text>
+        <Image source={require('../media/imgLogin.png')} style={{width: '100%', height: '50%', borderRadius: 5}} />
+        <Text style={{fontSize: 30}}>{item.stepNumber}</Text>
         <Text>{item.text}</Text>
       </View>
 
     )
   }
-  //console.log("RENDER: " + this.state.carouselItems);
+  //SI BIEN ME TRAE Y PUEDO VER LOS PASOS... NO ME LOS ESTÁ RENDERIZANDO...
+  //console.log(pasos);
+  //este map es para chequear que puedo ver los datos
+  pasos.map((itema) => {
+    console.log(itema.stepNumber);
+  })
+
   return (
-    <SafeAreaView style={{ flex: 1, paddingTop: 20 }}>
-      <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
+    <SafeAreaView style={{flex: 1, paddingTop: 20}}>
+      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
         <Carousel
           layout={"default"}
-          ref={ref => this.carousel = ref}
           data={pasos}
           sliderWidth={300}
           itemWidth={350}
           renderItem={renderItem}
-          onSnapToItem={index => this.setState({ activeIndex: index })} />
+          onSnapToItem={index => this.setState({activeIndex: index})} />
       </View>
     </SafeAreaView>
   );
 }
+export default Carousell;
+/*export default () => (
+    <IconComponentProvider IconComponent={MaterialCommunityIcons}>
+        <Carousell />
+    </IconComponentProvider>
+);*/
